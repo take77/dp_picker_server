349112f2af757c33006e0f484222f1b268f2bb8c
take77 <teke77@toki.waseda.jp>
2020-04-20 20:19:24 +0900

    feat: adjust display

--- db/migrate/20200403071308_dp_pokemons.rb
+++ db/migrate/20200403071308_dp_pokemons.rb
@@ -2,6 +2,7 @@ class DpPokemons < ActiveRecord::Migration[6.0]
   def change
     create_table :dp_pokemons do |t|
       t.string :name, null: false, default: ""
+      t.string :image
       t.integer :base_id, null: false, default: 0
       t.boolean :legend, null: false, default: false
       t.integer :weight, null: false, default: 0
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,15 @@
 require "csv"
+require 'carrierwave'
+require 'carrierwave/orm/activerecord'
 
 CSV.foreach('db/csv/dp_seed.csv', headers: true) do |row|
-	DpPokemon.create(
+	pokemon = DpPokemon.create(
 		name: row['name'],
 		base_id: row['base_id'],
 		legend: row['legend'],
 		weight: row['weight']
 		)
+
+	pokemon.image = open("db/images/#{pokemon.id}.png")
+	pokemon.save
 end
